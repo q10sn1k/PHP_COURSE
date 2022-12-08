91a36455371d941818017b5c8aaac049b80c6f68
q10sn1k <kondratenkona23@gmail.com>
2022-12-08 19:57:20 +0300

    add lessons

---
+++ lesson2/les1.php
@@ -0,0 +1,66 @@
+<?php
+$title = 'Урок 2';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $title ?></title>
+</head>
+<body>
+Добрый <b>вечер</b>, это Наш первый <i>текст</i>
+
+
+
+<ul>
+    <li>1-й пункт списка</li>
+    <li>2-й пункт списка</li>
+    <li>3-й пункт списка</li>
+    <li>4-й пункт списка</li>
+</ul>
+
+<ol>
+    <li>ЯП PHP</li>
+    <li>JS</li>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>SQL</li>
+</ol>
+
+<h1>Заголовок 1</h1>
+<h2>Заголовок 2</h2>
+<h3>Заголовок 3</h3>
+<h4>Заголовок 4</h4>
+<h5>Заголовок 5</h5>
+<h6>Заголовок 6</h6>
+
+<a href="https://ospanel.io/download/">Скачать Open Server</a>
+<a href="https://www.sublimetext.com/">Скачать SublimeText</a>
+
+<table border="1">
+    <tr>
+        <td>Ячейка 1</td>
+        <td>Ячейка 2</td>
+        <td>Ячейка 3</td>
+    </tr>
+
+    <tr>
+        <td>Ячейка 4</td>
+        <td>Ячейка 5</td>
+        <td>Ячейка 6</td>
+    </tr>
+
+    <tr>
+        <td>Ячейка 7</td>
+        <td>Ячейка 8</td>
+        <td>Ячейка 9</td>
+    </tr>
+    <!-- В HTML комеентарии пишутся так -->
+    <p>Параграф 1</p>
+    <p>Параграф 2</p>
+    <p>Параграф 3</p>
+</table>
+</body>
+</html>
